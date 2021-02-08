3263f62ec67ed781639a6340cb038796d914f0ae
Mahbub Mahi <mahi201539@gmail.com>
2021-02-08 23:21:09 +0600

    true vs false

--- true-false.js
+++ true-false.js
@@ -1,3 +1,14 @@
+// falsy:
+// 0
+// ""
+// undefined
+// null
+// NaN
+// false
+// truthy:
+// '0',' ',[],'false'
+
+
 const name = "Ashfak";
 if(name.length>0){
     console.log("condition is true");
