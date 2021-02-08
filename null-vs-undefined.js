1ee96a27fcfaf35917e52517f04a1fc9ee83fb77
Mahbub Mahi <mahi201539@gmail.com>
2021-02-08 23:52:43 +0600

    null-vs-undefined

---
+++ null-vs-undefined.js
@@ -0,0 +1,14 @@
+function add(num1,num2){
+    console.log(num1 + num2);
+    return
+}
+function add(num1,num2){
+    console.log(num1,num2);
+}
+
+const premik = {name:'Ashfak Mahbub',phone:0174526};
+console.log(premik.gf);
+
+let fun = undefined;
+console.log(fun);
+
